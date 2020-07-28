cd2f2b95c6aa09026d799d984692c7182ef835fe
mizael isaltino <mizael.isaltino@gmail.com>
2020-07-28 01:52:08 -0300

    Quase pronto

--- material-lifterlms.php
+++ material-lifterlms.php
@@ -8,7 +8,7 @@ Author: Mizael Isaltino
 add_action( 'add_meta_boxes', 'cd_meta_box_add' );
 function cd_meta_box_add()
 {
-    add_meta_box( 'material-lifterlms-meta-box-id', 'Material LifterLMS', 'cd_meta_box_cb', 'lesson', 'side', 'default' );
+    add_meta_box( 'material-lifterlms-meta-box-id', 'Material LifterLMS', 'cd_meta_box_cb', 'lesson', 'normal', 'default' );
 }
 
 function cd_meta_box_cb()
@@ -16,43 +16,110 @@ function cd_meta_box_cb()
     // $post is already set, and contains an object: the WordPress post
     global $post;
     $values = get_post_custom( $post->ID );
-    $link = isset( $values['meta_box_link'] ) ? $values['meta_box_link'][0] : '';
-    $titulo = isset( $values['meta_box_titulo'] ) ? $values['meta_box_titulo'][0] : '';
-    $check = isset( $values['meta_box_ativo'] ) ? esc_attr( $values['meta_box_ativo'][0] ) : '';
+    $titulo = isset( $values['meta_box_data'] ) ? $values['meta_box_data'][0] : '';
+    // print_r(values);
+    // die;
+    
      
     // We'll use this nonce field later on when saving.
     wp_nonce_field( 'my_meta_box_nonce', 'meta_box_nonce' );
     ?>
-    <div class="components-panel__row">
-      <div class="components-base-control">
-        <div class="components-base-control__field">
-          </br>
-          <input class="" type="checkbox" id="meta_box_ativo" name="meta_box_ativo" <?php checked( $check, true ); ?> />
-          <label class="components-checkbox-control__label" for="meta_box_ativo">Ativo</label>
-        </div>
-      </div>
-    </div>
-    <div class="components-panel__row">
-      <div class="components-base-control" style="width: 100%;">
-        <div class="components-base-control__field">
-          <label class="components-base-control__field" for="meta_box_titulo">Titulo</label>
-          <input class="components-text-control__input"  type="text" name="meta_box_titulo" id="meta_box_titulo"
-            value="<?php echo $titulo; ?>" />
-        </div>
-      </div>
-    </div>
+    
+    <input type="text" name="meta_box_data" id="meta_box_data" value='<?php echo $titulo; ?>' />
+  <br>
+  <table>
+    <tr class="linha" data-id="0">
+      <td>
+        <label>Ativo</label>
+        <input type="checkbox"  />
+      </td>
+      <td>
+        <label>Titulo</label>
+        <input type="text"  value="" />
+      </td>
+      <td>
+        <label>Link</label>
+        <input type="text"  value="" />
+      </td>
+      <td>
+        <button data-id="1" onclick="excluir(this)">Excluir</button>
+      </td>
+    </tr>
+  </table>
+  <button onclick="adicionar()">Adicionar</button>
+  <button onclick="salvar()">Salvar</button>
+  <button onclick="montar()">montar</button>
 
-    <div class="components-panel__row">
-      <div class="components-base-control" style="width: 100%;">
-        <div class="components-base-control__field">
-          <label class="components-base-control__field" for="meta_box_link">Link</label>
-          <input class="components-text-control__input"  type="text" name="meta_box_link" id="meta_box_link"
-            value="<?php echo $link; ?>" />
-        </div>
-      </div>
-    </div>
-     
-    <?php    
+  <script>
+
+    window.onload = montar();
+
+    function montar() {
+      var dados = document.querySelector("#meta_box_data").value;
+      //console.log(dados);
+
+      var valores = dados ? JSON.parse(atob(dados)) : [];
+
+      console.log(valores);
+
+      valores.forEach((valor, index) => {
+        //console.log(valores[index]);
+        
+        var linha = document.querySelector(`tr[data-id="${index}"]`);
+        var inputs = linha.querySelectorAll("input");
+
+        inputs[0].checked = valor.ativo;
+        inputs[1].value = valor.titulo;
+        inputs[2].value = valor.link;
+
+          adicionar();
+
+      });
+
+    }
+
+    function salvar() {
+      var linhas = document.querySelectorAll(".linha");
+      var dados = [];
+      linhas.forEach(linha => {
+        var inputs = linha.querySelectorAll("input");
+        if (inputs[1].value || inputs[2].value) {
+          var dado = { ativo: inputs[0].checked, titulo: inputs[1].value, link: inputs[2].value };
+          dados.push(dado);
+        }
+      });
+
+      var final = JSON.stringify(dados);
+      console.log(btoa(final));
+      document.querySelector("#meta_box_data").value = btoa(final);
+    }
+
+    function excluir(e) {
+      var id = e.getAttribute("data-id");
+      document.querySelector(`tr[data-id="${id}"]`).remove();
+    }
+
+    function adicionar() {
+      let tabela = document.querySelector("table");
+      let referencias = tabela.querySelectorAll("tr");
+      let referencia = referencias[referencias.length - 1];
+      var clone = referencia.cloneNode(true);
+      var id = clone.getAttribute("data-id");
+
+      clone.setAttribute("data-id", parseInt(id) + 1);
+      var btn = clone.querySelector("button");
+      var inputs = clone.querySelectorAll("input");
+
+      inputs.forEach(input => {
+        input.type != "checkbox" ? input.value = "" : input.checked = false;
+      });
+
+      btn.setAttribute("data-id", parseInt(id) + 1);
+      tabela.appendChild(clone);
+    }
+
+  </script>
+  <?php    
 }
 
 
@@ -70,22 +137,14 @@ function cd_meta_box_save( $post_id ){
     if( !current_user_can( 'edit_post' ) ) return;
 
     // now we can actually save the data
-    $allowed = array( 
-      'a' => array( // on allow a tags
-      'href' => array() // and those anchors can only have href attribute
-      )
-  );
-   
-  // Make sure your data is set before trying to save it
-  if( isset( $_POST['meta_box_link'] ) )
-      update_post_meta( $post_id, 'meta_box_link', wp_kses( $_POST['meta_box_link'], $allowed ) );
+    $allowed = "";
 
-    if( isset( $_POST['meta_box_titulo'] ) )
-      update_post_meta( $post_id, 'meta_box_titulo', wp_kses( $_POST['meta_box_titulo'], $allowed ) );
+    // print_r($_POST['meta_box_data']);
+    // die;
+      
+  if( isset( $_POST['meta_box_data'] ) )
+      update_post_meta( $post_id, 'meta_box_data', $_POST['meta_box_data']);
        
-  // This is purely my personal preference for saving check-boxes
-  $chk = isset( $_POST['meta_box_ativo'] ) && $_POST['meta_box_ativo'] ? true : false;
-  update_post_meta( $post_id, 'meta_box_ativo', $chk );
 }
 
 
@@ -105,7 +164,7 @@ function xai_my_class($content){
     <h5 class="llms-h5 llms-lesson-title llms-lesson-preview" style="font-size: 22px;">Material para download</h5>
     <ul style="margin-left: 15px;">
     <li>
-    <a href="' . $values['meta_box_link'][0] . '" target="_blank">' . $values['meta_box_titulo'][0] . '</a>
+    <a href="' . $values['meta_box_link'][0] . '" target="_blank">' . $values['meta_box_data'][0] . '</a>
     </li>
     </ul>
     </div>'; 
